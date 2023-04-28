67eccad744380bb1bdca7ab4434dc6dbf2b56890
MANUEL FABIAN OSUNA RAMIREZ <mosuna@tecmilenio.mx>
2023-04-27 22:05:12 -0700

    primera verssion

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@
     const cfg = {
 
         // Countdown Timer Final Date
-        finalDate : 'April 30, 2023 00:00:00',
+        finalDate : 'May 07, 2023 00:00:00',
         // MailChimp URL
         mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
 
